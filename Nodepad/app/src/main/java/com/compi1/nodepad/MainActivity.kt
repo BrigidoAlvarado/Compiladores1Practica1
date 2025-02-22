ca7b6041b29e91d5382362327c0026c4f46b0ad7
Brigido Alvarado <brigidoavarado@gmail.com>
2025-02-21 19:48:30 -0600

    se agrega el mecanismo para la creacion de nuevas celdas de texto

--- Nodepad/app/src/main/java/com/compi1/nodepad/MainActivity.kt
+++ Nodepad/app/src/main/java/com/compi1/nodepad/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -11,31 +12,39 @@ import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.main_layout)
-        val textButton: Button = findViewById(R.id.textButtom)
-        //Layout que esta en el scroll panel y que contendra a las celdas
+
+        //BOTON PARA AGREGAR UNA CELDA DE TEXTO
+        val addTexButton: Button = findViewById(R.id.textButton)
+        //BOTON PAR AGREGAR UNA CELDA DE CODIGO
+        val addCodeButton: Button = findViewById(R.id.codeButton)
+
+        //LAYOUT CONTENEDOR DE CELDAS
+        val cellsContainer: LinearLayout = findViewById(R.id.cells_container_layout)
 
 
-        textButton.setOnClickListener{
-            val cellsContainer: LinearLayout = findViewById(R.id.cells_container_layout)
-            textButton.setText("PRESIONADO")
-            val textTes: TextView = TextView(this).apply {
-                text = "Se ha creado una nueva celda"
-                textSize = 16f
-                setPadding(16,16,16,61)
-                layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-            }
-            cellsContainer.addView(textTes)
+        addTexButton.setOnClickListener{
+
+            // Se infla el layout que tiene el contenido de una celda
             val inflaterContainer: LayoutInflater = LayoutInflater.from(this)
             val newCell: View = inflaterContainer.inflate(R.layout.cell_layout, cellsContainer, false)
             cellsContainer.addView(newCell)
+
+            // Acceder a los elementos de la celda
+            val cellButton: Button = newCell.findViewById(R.id.execute_buttom)
+            val cellTex: TextView = newCell.findViewById(R.id.test_text)
+            val cellInputText: EditText = newCell.findViewById(R.id.inputTexteditText)
+
+            //Accion del boton de la celda
+            cellButton.setOnClickListener{
+
+                cellTex.setText("El boton de esta celda ya ha sido presionado \n NO ")
+            }
+
         }
 
     }
